20d98acb6cc38ad9d9aa57081c6d94da33c52fe2
Brian Fan and Ilene Gorski <ilene@>
2017-03-21 15:29:50 -0700

    Simplify toggle code

--- js/script2.js
+++ js/script2.js
@@ -1,18 +1,36 @@
-$(function(){
+// $(function(){
+//
+//   $(".javascript").click(function(){
+//     $(this).hide();
+//     $('#javascript').show();
+//   });
+//
+//   $("#javascript").click(function(){
+//     $(this).hide();
+//     $('.javascript').show();
+//   });
+//
+//   $(".operators").click(function(){
+//     $(this).hide();
+//     $('#operators').show();
+//   });
+//
+//   $("#operators").click(function(){
+//     $(this).hide();
+//     $('.operators').show();
+//   });
+// });
 
-  $(".javascript").click(function(){
-    $(".javascript").hide();
-    $('#javascript').show();
+
+// $(function(){
+//   $(".flashcard").click(function(){
+//     $('h3', this).toggle();
+//     $('p', this).toggle();
+//   });
+
+  $(function(){
+    $(".flashcard").click(function(){
+      $(this).children("p").toggle();
+      $(this).children("h3").toggle();
+    });
   });
-
-  $("#javascript").click(function(){
-    $("#javascript").hide();
-    $('.javascript').show();
-  });
-
-
-
-
-
-
-});
